15aca0e0880a9c0bd2bca7c10ba8cb4e47257f58
camilo <camilomontenegro71@gmail.com>
2025-01-30 23:21:33 -0500

    Funcion asignarTextoElemento

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigos = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementohtml = document.querySelector(elemento);
+    elementohtml.innerHTML = texto;
+    return;
+}
