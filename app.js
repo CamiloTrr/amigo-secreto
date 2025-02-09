6dd774f9564b4f536f53eb0988d105967ef2a58e
camilo <camilomontenegro71@gmail.com>
2025-02-09 18:53:40 -0500

    función para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
     if (nombreAmigo.length != 0) {
         amigos.push(nombreAmigo);
-        
+        actualizarListaAmigos();
         limpiarCampo();
     } else{
         alert("Por favor, inserte un nombre.");
     }
+}
+
+function actualizarListaAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML= "";
+    for (let i = 0; i<amigos.length; i++) {
+        let elemento = document.createElement("li")
+        elemento.textContent = amigos[i];
+        lista.appendChild(elemento);
+    }
+
 }
 
 function limpiarCampo() {
