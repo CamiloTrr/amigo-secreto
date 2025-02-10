496c08ea4900ac1d8903b6a253514a28caa76f8c
camilo <camilomontenegro71@gmail.com>
2025-02-09 20:32:12 -0500

    Versión final

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//creacion del array
 let amigos = [];
 
+//funcion para el boton añadir
 function agregarAmigo() {
     let nombreAmigo = document.getElementById('amigo').value;
+    //si el tamaño del array es diferente de 0 sigue con las acciones
     if (nombreAmigo.length != 0) {
         amigos.push(nombreAmigo);
         actualizarListaAmigos();
@@ -12,10 +15,13 @@ function agregarAmigo() {
     }
 }
 
+//se agrega los elementos añadidos al primer ul
 function actualizarListaAmigos() {
+    //limpiar la lista existente
     let lista = document.getElementById('listaAmigos');
     lista.innerHTML= "";
     for (let i = 0; i<amigos.length; i++) {
+        //se crea un nuevo elemento <li> para cada amigo
         let elemento = document.createElement("li")
         elemento.textContent = amigos[i];
         lista.appendChild(elemento);
@@ -23,6 +29,7 @@ function actualizarListaAmigos() {
 
 }
 
+//se realiza el sorteo del amigo y se imprime en el segundo ul
 function sortearAmigo() {
     
     if (amigos.length != 0) {
@@ -39,6 +46,7 @@ function sortearAmigo() {
     
 }
 
+//limpia el input
 function limpiarCampo() {
     document.getElementById('amigo').value = '';
 }
